2ca3ac2d0ba65af378f281c6df5db9057c46bb33
husseinjoe <hussein.joe.au@gmail.com>
2018-11-18 20:25:44 +1100

    Try to use network_mode: service to have the pause container as a service in the same compose file. It did not work
    docker-compose up
    Creating network "security_default" with the default driver
    Creating pause_container ... done
    Creating zookeeper1      ... error
    
    ERROR: for zookeeper1  Cannot start service zookeeper1: linux spec namespaces: Invalid IPC mode: service:pause_container
    
    ERROR: for zookeeper1  Cannot start service zookeeper1: linux spec namespaces: Invalid IPC mode: service:pause_container
    ERROR: Encountered errors while bringing up the project.

--- scripts.sh
+++ scripts.sh
@@ -7,10 +7,10 @@ docker run -d --name pause_container \
     -p 2183:2183 \
     gcr.io/google_containers/pause-amd64:3.0
 
-docker exec kafka kafka-topics --zookeeper localhost:2181,localhost:2182,localhost:2183 --create --topic test --partitions 1 --replication-factor 1
+docker exec kafka_broker_1 kafka-topics --zookeeper localhost:2181,localhost:2182,localhost:2183 --create --topic test --partitions 1 --replication-factor 1
 
-docker exec -it kafka kafka-console-producer --broker-list localhost:9092,localhost:9093,localhost:9094 --topic test
-docker exec -it security_broker1_1 kafka-console-consumer --bootstrap-server localhost:9092,localhost:9093,localhost:9094 --topic test --from-beginning
+docker exec -it kafka_broker_1 kafka-console-producer --broker-list localhost:9092,localhost:9093,localhost:9094 --topic test
+docker exec -it kafka_broker_1 kafka-console-consumer --bootstrap-server localhost:9092,localhost:9093,localhost:9094 --topic test --from-beginning
 
 docker run -it --net=container:pause_container --ipc=container:pause_container \
 --pid=container:pause_container     confluentinc/cp-kafka:5.0.0  kafka-console-producer \
